a0628be1795d6e5cca0e59f6fac87dfee2cd4cac
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2024-09-10 14:41:16 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1220 ms, Memory: 307376 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int repeat = Integer.parseInt(br.readLine().trim());
+      for(int i=0; i<repeat; i++) {
+        int studentCount = Integer.parseInt(br.readLine().trim());
+        int[] choices = Stream.of(("0 " + br.readLine().trim()).split(" "))
+                              .mapToInt(Integer::parseInt)
+                              .toArray();
+        Dfs dfs = new Dfs(choices);
+        System.out.println(dfs.getResult());
+      }
+  }
+}
+
+class Dfs {
+  int[] choices;
+  int[] visited;
+  Stack<Integer> stack = new Stack<>();
+  int answer = 0;
+  
+  public Dfs(int[] choices) {
+    this.choices = choices;
+    this.visited = new int[choices.length];
+  }
+  
+  public int getResult() {
+    for(int i=1; i<choices.length; i++) {
+      if(visited[i] != 0) continue;
+      dfs(i);
+    }
+    for(int v: visited) {
+      if(v==3) answer++;
+    }
+    return answer;
+  }
+  
+  private void dfs(int choice) {
+    visited[choice] = 1;
+    stack.push(choice);
+    
+    int next = choices[choice];
+    switch(visited[next]) {
+      case 0: 
+        dfs(next);
+        break;
+      
+      case 1:
+        while(true) {
+          int last = stack.pop();
+          visited[last] = 2;
+          if(last == next) break;
+        };
+        
+      case 2:
+      case 3:
+        while(!stack.empty()) {
+          int last = stack.pop();
+          visited[last] = 3;
+        };
+        break;
+        
+      default:
+        break;
+    }
+  }
+}
