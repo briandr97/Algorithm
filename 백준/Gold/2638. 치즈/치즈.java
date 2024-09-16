6ed7bdabfeb44088dc32e546135026e08b857acd
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2024-09-16 14:30:03 +0900

    [Gold III] Title: 치즈, Time: 208 ms, Memory: 18712 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -4,18 +4,18 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
+
         int[][] map = new int[n][m];
-        
+
         for(int i=0; i<n; i++) {
             int[] row = Stream.of(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
             map[i] = row;
         }
-        
+
         Graph graph = new Graph(map);
         System.out.println(graph.solution());
     }
@@ -23,70 +23,73 @@ class Main {
 
 class Graph {
     int[][] map;
-    boolean[][] gonnaBeMelt;
-    int outsideLabel = 2;
+    boolean[][] gonnaBeMelted;
     int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
+    int outsideLabel = 2;
+
     public Graph(int[][] map) {
-        this.map = map;    
+        this.map = map;
     }
-    
+
     private int getWidth() {
         return map[0].length;
     }
-    
+
     private int getHeight() {
         return map.length;
     }
-        
+    
+    private boolean isOutOfIndex(int column, int row) {
+        return column < 0 || row < 0 || column >= getHeight() || row >= getWidth();
+    }
+
     public int solution() {
         int time = 0;
         while(true) {
-            gonnaBeMelt = new boolean[getHeight()][getWidth()];
+            gonnaBeMelted = new boolean[getHeight()][getWidth()];
             dfsOutsideCheese(0, 0);
             int cheeseCount = 0;
             for(int i=0; i<map.length; i++) {
                 for(int j=0; j<map[0].length; j++) {
                     if(map[i][j] != 1) continue;
-                    if(isMeltedCheese(i, j)) cheeseCount++;
+                    if(isMeltingCheese(i, j)) cheeseCount++;
                 }
             }
             for(int i=0; i<map.length; i++) {
                 for(int j=0; j<map[0].length; j++) {
-                    if(gonnaBeMelt[i][j]) map[i][j] = outsideLabel;
+                    if(gonnaBeMelted[i][j]) map[i][j] = outsideLabel;
                 }
             }
             if(cheeseCount==0) break;
             time++;
             outsideLabel++;
-            cheeseCount = 0;
         }
         return time;
     }
-    
+
     public void dfsOutsideCheese(int column, int row) {
-        if(column<0 || row<0 || column>=map.length || row>=map[0].length) return;
+        if(isOutOfIndex(column, row)) return;
         if(map[column][row] == 1) return;
         if(map[column][row] == outsideLabel) return;
-        
+
         map[column][row] = outsideLabel;
-        for(int i=0; i<direction.length; i++) {
-            dfsOutsideCheese(column + direction[i][0], row + direction[i][1]);
+        for (int[] d : direction) {
+            dfsOutsideCheese(column + d[0], row + d[1]);
         }
     }
-    
-    public boolean isMeltedCheese(int column, int row) {
-        if(column<0 || row<0 || column>=getHeight() || row>=getWidth()) return false;
+
+    public boolean isMeltingCheese(int column, int row) {
+        if(isOutOfIndex(column, row)) return false;
+        
         int labelCount=0;
-        for(int i=0; i<direction.length; i++) {
-            int nextColumn = column + direction[i][0];
-            int nextRow = row + direction[i][1];
-            if(nextColumn<0 || nextRow<0 || nextColumn>=getHeight() || nextRow>=getWidth()) continue;
-            int label = map[nextColumn][nextRow];
-            if(label == outsideLabel) labelCount++;
+        for (int[] d : direction) {
+            if(isOutOfIndex(column + d[0], row + d[1])) continue;
+            int label = map[column + d[0]][row + d[1]];
+            if (label == outsideLabel) labelCount++;
         }
+        
         if(labelCount >= 2) {
-            gonnaBeMelt[column][row] = true;
+            gonnaBeMelted[column][row] = true;
             return true;
         }
         return false;
