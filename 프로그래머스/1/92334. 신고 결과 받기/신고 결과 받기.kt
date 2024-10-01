53d4b32105ef43e44de8247052dca72ca4cbd956
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2024-10-02 03:52:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 198.20 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
@@ -1,32 +1,14 @@
-class Solution {
-    val indexMap = mutableMapOf<String, Int>()
-    
-    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
-        var reported = MutableList(id_list.size) { 0 }
-        val answer = IntArray(id_list.size) { 0 }
-        initMap(id_list)
-        
-        val int_id_list = report.map { 
-            val arr = it.split(" ")
-            indexMap[arr[0]]!! to indexMap[arr[1]]!!
-        }.distinct()
-            
-        int_id_list.forEach { reported[it.second] += 1 }
-        reported = reported.mapIndexedNotNull {idx: Int, e: Int ->
-            if(e>=k) idx
-            else null
-        }.toMutableList()
-        
-        int_id_list.forEach {
-            if(it.second in reported) answer[it.first] += 1
-        }
-        
-        return answer
-    }
-    
-    private fun initMap(id_list: Array<String>) {
-        id_list.forEachIndexed { idx: Int, e: String ->
-            indexMap[e] = idx
-        }
+class Solution {      
+    fun solution(idList: Array<String>, report: Array<String>, k: Int): IntArray {
+        return report.asSequence()
+            .distinct()
+            .map { it.split(" ").run { get(0) to get(1) }}
+            .groupBy { it.second }
+            .filter { it.value.size >= k }
+            .map { it.value }
+            .flatten()
+            .groupingBy { it.first }
+            .eachCount()
+            .run { idList.map { getOrDefault(it, 0) }.toIntArray() }
     }
 }
