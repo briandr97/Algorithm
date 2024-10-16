6d94a3e6768b942799289bf3f5b00a1c248b545a
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2024-10-17 02:11:01 +0900

    [level 2] Title: 의상, Time: 0.33 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.kt
+++ 프로그래머스/2/42578. 의상/의상.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-        val map = mutableMapOf<String, Int>()
-        clothes.forEach { clothe -> 
-            val key = clothe[1]
-            map[key] = map.getOrDefault(key, 0) + 1
-        }
-        
-        return map.values.fold(1) { acc, count -> acc * (count + 1)} - 1
+        return clothes.groupBy { it[1] }.values.fold(1) { acc, e -> acc * (e.size + 1) } - 1
     }
 }
