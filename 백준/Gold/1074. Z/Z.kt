5e52288ebaa0af817e8011f149a48aa23fa267b5
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2024-10-28 21:15:17 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 15024 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.kt
@@ -0,0 +1,43 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine().trim())
+    val N = st.nextToken().toInt()
+    val R = st.nextToken().toInt()
+    val C = st.nextToken().toInt()
+
+    val boardSize = Math.pow(2.0, N.toDouble()).toInt()
+    var count = 0
+
+    fun solution(sr: Int, sc: Int, size: Int) {
+        if (sr == R && sc == C) {
+            println(count)
+            return
+        }
+        if (size == 0) return
+
+        val rowDivider = sr + size - 1
+        val columnDivider = sc + size - 1
+        val area = if (R > rowDivider) {
+            if (C > columnDivider) 3
+            else 2
+        } else {
+            if (C > columnDivider) 1
+            else 0
+        }
+
+        count += size * size * area
+
+        when (area) {
+            0 -> solution(sr, sc, size / 2)
+            1 -> solution(sr, sc + size, size / 2)
+            2 -> solution(sr + size, sc, size / 2)
+            3 -> solution(sr + size, sc + size, size / 2)
+        }
+    }
+
+    solution(0, 0, boardSize / 2)
+}
