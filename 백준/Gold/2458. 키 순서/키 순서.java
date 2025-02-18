7261fd76893292d1ceed1fab6b84ba60f176a794
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2025-02-18 23:52:34 +0900

    [Gold IV] Title: 키 순서, Time: 832 ms, Memory: 37032 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -3,91 +3,43 @@ import java.util.stream.*;
 import java.io.*;
 
 public class Main {
-	static List<List<Integer>> toMe;
-	static List<Set<Integer>> totalToMe;
-	static List<List<Integer>> fromMe;
-	static List<Set<Integer>> totalFromMe;
-	static boolean[] toVisited;
-	static boolean[] fromVisited;
-	static int[] connected;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		init(n);
-
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			fromMe.get(a).add(b);
-			toMe.get(b).add(a);
-		}
-
-		for (int i = 1; i <= n; i++) {
-			calculateTotalToMe(i);
-			int to = totalToMe.get(i).size();
-			calculateTotalFromMe(i);
-			int from = totalFromMe.get(i).size();
-			connected[i] = to + from;
-		}
-
-		int count = 0;
-		for (int i = 1; i <= n; i++) {
-			if (connected[i] == n - 1) {
-				count++;
-			}
-		}
-
-		System.out.println(count);
-	}
-
-	static void calculateTotalToMe(int idx) {
-		if (toVisited[idx])
-			return;
-
-		for (int child : toMe.get(idx)) {
-			if (!toVisited[child]) {
-				calculateTotalToMe(child);
-			}
-			totalToMe.get(idx).add(child);
-			totalToMe.get(idx).addAll(totalToMe.get(child));
-		}
-		toVisited[idx] = true;
-	}
-
-	static void calculateTotalFromMe(int idx) {
-		if (fromVisited[idx])
-			return;
-
-		for (int parent : fromMe.get(idx)) {
-			if (!fromVisited[parent]) {
-				calculateTotalFromMe(parent);
-			}
-			totalFromMe.get(idx).add(parent);
-			totalFromMe.get(idx).addAll(totalFromMe.get(parent));
-		}
-		fromVisited[idx] = true;
-	}
-
-	static void init(int n) {
-		toMe = new ArrayList<>();
-		fromMe = new ArrayList<>();
-		totalToMe = new ArrayList<>();
-		totalFromMe = new ArrayList<>();
-		toVisited = new boolean[n + 1];
-		fromVisited = new boolean[n + 1];
-		connected = new int[n + 1];
-
-		for (int i = 0; i <= n; i++) {
-			toMe.add(new ArrayList<>());
-			fromMe.add(new ArrayList<>());
-			totalFromMe.add(new HashSet<>());
-			totalToMe.add(new HashSet<>());
-		}
-	}
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      
+      boolean[][] paths = new boolean[n + 1][n + 1];
+      
+      for(int i=0; i<m; i++) {
+          st = new StringTokenizer(br.readLine());
+          int v1 = Integer.parseInt(st.nextToken());
+          int v2 = Integer.parseInt(st.nextToken());
+          paths[v1][v2] = true;
+      }
+      
+      for(int k=1; k<=n; k++) {
+          for(int i=1; i<=n; i++) {
+              for(int j=1; j<=n; j++) {
+                  if(i == j) continue;
+                  if(paths[i][j]) continue;
+                  if(!paths[i][k] || !paths[k][j]) continue;
+                  paths[i][j] = true;
+              }
+          }
+      }
+      
+      int count = 0;
+      for(int i=1; i<=n; i++) {
+          int sum = 0;
+          for(int j=1; j<=n; j++) {
+              if(paths[i][j]) sum++;
+              if(paths[j][i]) sum++;
+          }
+          if(sum == n - 1) count++;
+      }
+      
+      System.out.println(count);
+  }
 }
