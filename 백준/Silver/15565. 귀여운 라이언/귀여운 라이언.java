1ded08d1dce2639f93690e8e0bed7a0f2fdcf9a7
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2025-02-19 16:54:42 +0900

    [Silver I] Title: 귀여운 라이언, Time: 364 ms, Memory: 73696 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Main {
     
-    // 투포인터로 풀기
+    // 슬라이딩 윈도우로 풀기
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,22 +13,18 @@ public class Main {
         final int APEACH = 2; 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] dolls = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dolls = new int[n];
+        int lionCount = 0;
         
-        int left = 0;
-        int right = 0;
-        int count = 0;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            if(Integer.parseInt(st.nextToken()) == LION) dolls[lionCount++] = i;
+        }
+        
         int min = 1000001;
-        
-        while(right < n) {
-            if(dolls[right] == LION) count++;
-            
-            while(count >= k) {
-                min = Math.min(min, right - left + 1);
-                if(dolls[left++] == LION) count--;
-            }
-            
-            right++;
+        for(int i=k-1; i<lionCount; i++) {
+            int gap = dolls[i] - dolls[i - k + 1] + 1;
+            min = Math.min(min, gap);
         }
         
         if(min == 1000001) min = -1;
