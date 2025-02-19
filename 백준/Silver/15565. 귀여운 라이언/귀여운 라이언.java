77bcca3d1ae9989e861b25c7c91828c46073ddeb
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2025-02-19 16:38:46 +0900

    [Silver I] Title: 귀여운 라이언, Time: 404 ms, Memory: 90964 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -4,39 +4,34 @@ import java.io.*;
 
 public class Main {
     
+    // 투포인터로 풀기
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      
-      final int LION = 1;
-      final int APEACH = 2; 
-      int n = Integer.parseInt(st.nextToken());
-      int k = Integer.parseInt(st.nextToken());
-      int[] dolls = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      int[] lionCountAccum = new int[n];
-      for(int i=0; i<n; i++) {
-          if(dolls[i] == LION) lionCountAccum[i]++;
-          if(i > 0) lionCountAccum[i] += lionCountAccum[i - 1];
-      }
-      
-      int[] compressedCount = new int[lionCountAccum[n - 1] + 1];
-      int idx = 1;
-      for(int i=0; i<n; i++) {
-          if(lionCountAccum[i] == idx) {
-              compressedCount[idx++] = i;
-          }
-      }
-      
-      int min = 1000001;
-      for(int i=compressedCount.length - 1; i>0; i--) {
-          if(i < k) break;
-          int last = compressedCount[i];
-          int start = compressedCount[i - k + 1];
-          int result = last - start + 1;
-          min = Math.min(min, result);
-      }
-      
-      if(min == 1000001) min = -1;
-      System.out.println(min);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        final int LION = 1;
+        final int APEACH = 2; 
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] dolls = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int min = 1000001;
+        
+        while(right < n) {
+            if(dolls[right] == LION) count++;
+            
+            while(count >= k) {
+                min = Math.min(min, right - left + 1);
+                if(dolls[left++] == LION) count--;
+            }
+            
+            right++;
+        }
+        
+        if(min == 1000001) min = -1;
+        System.out.println(min);
     }
 }
