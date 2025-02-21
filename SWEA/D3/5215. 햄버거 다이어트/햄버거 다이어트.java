02e7739162ce470a7fcc0b7afab192ad43fd142b
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2025-02-21 14:48:16 +0900

    [D3] Title: 햄버거 다이어트, Time: 164 ms, Memory: 20,072 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,50 +3,38 @@ import java.util.stream.*;
 import java.io.*;
 
 public class Solution {
-    static int max;
-    static int ingredientCount;
-    static int calorieLimit;
-    static int[] scores;
-    static int[] calories; 
-    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = null;
-        
-        int test = Integer.parseInt(br.readLine());
-        for(int tc=1; tc<=test; tc++) {
-            sb.append("#").append(tc).append(" ");
-            
-            st = new StringTokenizer(br.readLine());
-            ingredientCount = Integer.parseInt(st.nextToken());
-            calorieLimit = Integer.parseInt(st.nextToken());
-            max = 0;
-            
-            scores = new int[ingredientCount];
-            calories = new int[ingredientCount];
-            for(int i=0; i<ingredientCount; i++) {
-                st = new StringTokenizer(br.readLine());
-                scores[i] = Integer.parseInt(st.nextToken());
-                calories[i] = Integer.parseInt(st.nextToken());
-            }
-            
-            comb(0, 0, 0);
-            sb.append(max).append("\n");
-        }
-        
-        System.out.println(sb);
-    }
-    
-    static void comb(int cnt, int scoreSum, int calorieSum) {
-        if(calorieSum > calorieLimit) return;
-        max = Math.max(max, scoreSum);
-        if(ingredientCount == cnt) return;
-        
-        // 이 재료를 넣는다.
-        comb(cnt + 1, scoreSum + scores[cnt], calorieSum + calories[cnt]);
-        
-        // 이 재료를 넣지 않는다.
-        comb(cnt + 1, scoreSum, calorieSum);
-    }
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringBuilder sb = new StringBuilder();
+      StringTokenizer st = null;
+      
+      int test = Integer.parseInt(br.readLine());
+      for(int tc=1; tc<=test; tc++) {
+          sb.append("#").append(tc).append(" ");
+          
+          st = new StringTokenizer(br.readLine());
+          int ingredientCount = Integer.parseInt(st.nextToken());
+          int calorieLimit = Integer.parseInt(st.nextToken());
+          
+          int[] scores = new int[ingredientCount];
+          int[] calories = new int[ingredientCount];
+          for(int i=0; i<ingredientCount; i++) {
+              st = new StringTokenizer(br.readLine());
+              scores[i] = Integer.parseInt(st.nextToken());
+              calories[i] = Integer.parseInt(st.nextToken());
+          }
+          
+          int[] dp = new int[calorieLimit + 1];
+          for(int i=0; i<ingredientCount; i++) {
+              for(int j=calorieLimit; j>0; j--) {
+                  if(calories[i] > j) continue;
+                  dp[j] = Math.max(dp[j], dp[j - calories[i]] + scores[i]);
+              }
+          }
+          
+          sb.append(dp[calorieLimit]).append("\n");
+      }
+      
+      System.out.println(sb);
+  }
 }
