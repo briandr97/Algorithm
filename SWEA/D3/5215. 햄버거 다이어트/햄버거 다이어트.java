f051a5e6b5f5124831c841023b74f42cc277f14b
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2025-02-21 09:48:50 +0900

    [D3] Title: 햄버거 다이어트, Time: 164 ms, Memory: 20,072 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,42 +1,47 @@
 import java.util.*;
+import java.util.stream.*;
 import java.io.*;
- 
-class Solution {
-    private static int n;
-    private static int limit;
-    private static int[][] ingredients;
-    private static int max;
-     
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        StringBuilder sb = new StringBuilder();
-         
-        int T = Integer.parseInt(br.readLine());
-        for(int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            limit = Integer.parseInt(st.nextToken());
-            ingredients = new int[n][2];
-            max = 0;
-             
-            for(int i=0; i<n; i++) {
-                st = new StringTokenizer(br.readLine());
-                ingredients[i][0] = Integer.parseInt(st.nextToken());
-                ingredients[i][1] = Integer.parseInt(st.nextToken());
-            }
-             
-            dfs(0, 0, 0);
-            sb.append("#").append(tc).append(" ").append(max).append("\n");
-        }
-        System.out.print(sb.toString());
-    }
-    private static void dfs(int score, int sum, int idx) {
-        if(sum > limit) return;
-        if(score > max) max = score;
- 
-        for(int i=idx; i<n; i++) {
-            dfs(score + ingredients[i][0], sum + ingredients[i][1], i + 1);
-        }
-    }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringBuilder sb = new StringBuilder();
+      StringTokenizer st = null;
+      
+      int test = Integer.parseInt(br.readLine());
+      for(int tc=1; tc<=test; tc++) {
+          sb.append("#").append(tc).append(" ");
+          st = new StringTokenizer(br.readLine());
+          int ingredientCount = Integer.parseInt(st.nextToken());
+          int calorieLimit = Integer.parseInt(st.nextToken());
+          int[] scores = new int[ingredientCount + 1];
+          int[] calories = new int[ingredientCount + 1];
+          
+          for(int i=1; i<=ingredientCount; i++) {
+              st = new StringTokenizer(br.readLine());
+              scores[i] = Integer.parseInt(st.nextToken());
+              calories[i] = Integer.parseInt(st.nextToken());
+          }
+          
+          int[][] dp = new int[ingredientCount + 1][calorieLimit + 1];
+          for(int i=1; i<=ingredientCount; i++) {
+              for(int j=1; j<=calorieLimit; j++) {
+                  // 이 재료를 넣지 않는다.
+                  dp[i][j] = dp[i - 1][j];
+                  
+                  // 이 재료를 넣는다.
+                  int score = scores[i];
+                  int calorie = calories[i];
+                  if(calorie > j) continue;
+                  score = dp[i-1][j - calorie] + score;
+                  
+                  dp[i][j] = Math.max(dp[i][j], score);
+              } 
+          }
+          
+          sb.append(dp[ingredientCount][calorieLimit]).append("\n");
+      }
+      
+      System.out.println(sb);
+  }
 }
