790f5a1e7be5c7f84b3dcfbbc1698fd140c9564f
Kwon Yongmin <78788847+briandr97@users.noreply.github.com>
2025-03-06 17:10:42 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 268 ms, Memory: 46,912 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,81 +1,89 @@
-import java.util.*;
-import java.io.*;
-
-public class Solution {
-    static int N;
-    static char[][] board;
-    static boolean[][] visited;
-    static int[][] direction = {{1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1}};
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int test = Integer.parseInt(br.readLine());
-        for(int tc=1; tc<=test; tc++) {
-            sb.append("#").append(tc).append(" ");
-            N = Integer.parseInt(br.readLine());
-            
-            board = new char[N][N];
-            for(int i=0; i<N; i++) {
-                String str = br.readLine();
-                for(int j=0; j<N; j++) {
-                    board[i][j] = str.charAt(j);
+import java.util.Scanner;
+import java.io.FileInputStream;
+  
+class Solution {
+    private static final int TRAP = -2;
+    private static final int DOT = -1;
+    private static int n;
+    private static int[][] board;
+    private static boolean[][] visited;
+    private static int[][] direction = {{1, 1}, {1, 0}, {1, -1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, 1}, {0, -1}};
+      
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+  
+        for(int test_case = 1; test_case <= T; test_case++) {
+            // 입력
+            n = sc.nextInt();
+            board = new int[n][n];
+            visited = new boolean[n][n];
+            for(int i=0; i<n; i++) {
+                String input = sc.next();
+                for(int j=0; j<n; j++) {
+                    if(input.charAt(j) == '*') board[i][j] = TRAP;
+                    else if(input.charAt(j) == '.') board[i][j] = DOT;
                 }
             }
-            
-            int zeroCount = 0;
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<N; j++) {
-                    if(board[i][j] != '.') continue;
-                    setBombCount(i, j);
+              
+            // 구현
+            for(int i=0; i<n; i++) { // 모든 칸 체크하고 숫자 대입 + visitied에 지뢰 칸 체크
+                for(int j=0; j<n; j++) {
+                    checkTrap(i, j);
+                    if(board[i][j] == TRAP) visited[i][j] = true;
                 }
             }
-            
+              
             int clickCount = 0;
-            visited = new boolean[N][N];
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<N; j++) {
-                    if(board[i][j] != '0' || visited[i][j]) continue;
+            for(int i=0; i<n; i++) { // 0 클릭 횟수 세기
+                for(int j=0; j<n; j++) {
+                    if(visited[i][j]) continue;
+                    if(board[i][j] != 0) continue;
+                    clickCount++;
                     dfs(i, j);
+                }
+            }
+              
+            for(int i=0; i<n; i++) { // 남은 공간 클릭 횟수 세기
+                for(int j=0; j<n; j++) {
+                    if(visited[i][j]) continue;
                     clickCount++;
                 }
             }
-            
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<N; j++) {
-                    if(board[i][j] == '*' || visited[i][j]) continue;
-                    clickCount++;
-                }
-            }
-            
-            sb.append(clickCount).append("\n");
+  
+            // 출력
+            System.out.println("#" + test_case + " " + clickCount);
         }
-        
-        System.out.println(sb);
     }
-    
-    static void setBombCount(int row, int column) {
-        char sum = '0';
+      
+    private static void dfs(int row, int column) {
+        if(visited[row][column]) return;
+        visited[row][column] = true;
+        if(board[row][column] != 0) return;
+          
         for(int[] d: direction) {
-            int nRow = row + d[0];
-            int nColumn = column + d[1];
-            if(nRow < 0 || nRow >= N || nColumn < 0 || nColumn >= N) continue;
-            if(board[nRow][nColumn] == '*') sum++;
+            int nextRow = row + d[0];
+            int nextColumn = column + d[1];
+            if(isOut(nextRow, nextColumn)) continue;
+            dfs(nextRow, nextColumn);
+        }
+    }
+      
+    private static void checkTrap(int row, int column) {
+        if(board[row][column] != DOT) return;
+        int sum = 0;
+        for(int[] d: direction) {
+            int nextRow = row + d[0];
+            int nextColumn = column + d[1];
+            if(isOut(nextRow, nextColumn)) continue;
+            if(board[nextRow][nextColumn] == TRAP) sum += 1;
         }
         board[row][column] = sum;
     }
-    
-    static void dfs(int row, int column) {
-        visited[row][column] = true;
-        if(board[row][column] != '0') return;
-        
-        for(int[] d: direction) {
-            int nRow = row + d[0];
-            int nColumn = column + d[1];
-            if(nRow < 0 || nRow >= N || nColumn < 0 || nColumn >= N) continue;
-            if(board[nRow][nColumn] == '*' || visited[nRow][nColumn]) continue;
-            dfs(nRow, nColumn);
-        }
+      
+    private static boolean isOut(int row, int column) {
+        if(row < 0 || row >= n) return true;
+        if(column < 0 || column >= n) return true;
+        return false;
     }
 }
